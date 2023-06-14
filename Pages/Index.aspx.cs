0602e43bdab387a247272f64f2023771683d900e
Juan Aristizábal <juanaristi2001@gmail.com>
2023-06-14 17:29:55 -0500

    Agregar Log

--- Pages/Index.aspx.cs
+++ Pages/Index.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using CRUD.Utilities;
@@ -34,6 +35,25 @@ namespace CRUD.Pages
 
         protected void BtnCreate_Click(object sender, EventArgs e)
         {
+            // Obtener la fecha y hora actual
+            DateTime fechaActual = DateTime.Now;
+
+            // Obtener la dirección IP del cliente
+            string direccionIP = Request.UserHostAddress;
+
+            // Crear la cadena de texto a guardar en el archivo de registro
+            string logText = $"{fechaActual}|{direccionIP}|CREATE";
+
+            // Obtener la ruta completa del archivo de registro
+            string rutaArchivoLog = Server.MapPath("~/Logs/log.txt");
+
+            // Escribir la cadena de texto en el archivo de registro
+            using (StreamWriter sw = File.AppendText(rutaArchivoLog))
+            {
+                sw.WriteLine(logText);
+            }
+
+            // Redireccionar a la página CRUD.aspx con el parámetro 'op=C'
             Response.Redirect("~/Pages/CRUD.aspx?op=C");
         }
 
